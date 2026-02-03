df735318662211130c9ba78aa438e234011b5d7c
ignacio.arguello@yahoo.com.ar <ignacio.arguello@yahoo.com.ar>
2026-02-03 05:42:35 -0300

    agregando html

--- app.js
+++ app.js
@@ -1,2 +1,10 @@
-const express = requiere ('express');
-const app = express();
+let express = require ('express');
+const path = require ('path');
+let app = express();
+app.listen (3000, () => console.log('servidor corriendo')
+)
+app.get('/', (req,res) => {
+ const HTML =  res.sendFile(path.resolve(__dirname, './views/index.html'))
+   ;
+
+});
